916214d1490050cb28857533b727383a58d8cc8f
Bruno Istvan Campos Monteiro <bruno.istvan@gmail.com>
2022-08-14 09:21:20 -0300

    curso python

---
+++ while-else.py
@@ -0,0 +1,53 @@
+# Sem while-else
+
+basket = [
+    {'fruit': 'apple', 'qty': 20},
+    {'fruit': 'banana', 'qty': 30},
+    {'fruit': 'orange', 'qty': 10}
+]
+
+fruit = input('Enter a fruit:')
+
+index = 0
+found_it = False
+
+while index < len(basket):
+    item = basket[index]
+    # check the fruit name
+    if item['fruit'] == fruit:
+        found_it = True
+        print(f"The basket has {item['qty']} {item['fruit']}(s)")
+        break
+
+    index += 1
+
+if not found_it:
+    qty = int(input(f'Enter the qty for {fruit}:'))
+    basket.append({'fruit': fruit, 'qty': qty})
+    print(basket)
+    
+    
+# Com while-else
+
+basket = [
+    {'fruit': 'apple', 'qty': 20},
+    {'fruit': 'banana', 'qty': 30},
+    {'fruit': 'orange', 'qty': 10}
+]
+
+fruit = input('Enter a fruit:')
+
+index = 0
+
+while index < len(basket):
+    item = basket[index]
+    # check the fruit name
+    if item['fruit'] == fruit:
+        print(f"The basket has {item['qty']} {item['fruit']}(s)")
+        break
+
+    index += 1
+else:
+    qty = int(input(f'Enter the qty for {fruit}:'))
+    basket.append({'fruit': fruit, 'qty': qty})
+    print(basket)
